1501dbc52c39942ad5eeba0d25f25eb6c14564dc
vulong008 <84512747+vulong008@users.noreply.github.com>
2022-06-28 21:25:54 +0700

    Init

---
+++ app/src/main/java/com/example/activitylife/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.activitylife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+TextView txt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("AAA","chạy vào onCreate ");
+        txt=findViewById(R.id.txt);
+        txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("AAA","chạy vào onRestart ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("AAA","chạy vào onStart ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("AAA","chạy vào onStop ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("AAA","chạy vào onDestroy ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("AAA","chạy vào onPause ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("AAA","chạy vào onResume ");
+    }
+}
