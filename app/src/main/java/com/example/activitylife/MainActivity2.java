9e1e8775edce07ffad68654e78e6fb744c77180f
vulong008 <84512747+vulong008@users.noreply.github.com>
2022-07-11 00:03:20 +0700

    long

--- app/src/main/java/com/example/activitylife/MainActivity2.java
+++ app/src/main/java/com/example/activitylife/MainActivity2.java
@@ -19,6 +19,10 @@ public class MainActivity2 extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity2.this, MainActivity.class);
                 startActivity(intent);
+
+
+
+                
             }
         });
     }
